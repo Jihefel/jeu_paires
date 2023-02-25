c60b829fc87d204eb195cbcee0cc541065ee7baa
Jihefel <jfl.jflament@gmail.com>
2023-02-25 20:39:21 +0100

    correction bug chemins relatifs

--- public/js/modules/instances.js
+++ public/js/modules/instances.js
@@ -1,70 +1,70 @@
 import { Musique } from "./classes.js";
 
 export const takinItBack = new Musique(
-  "../../public/img/hhz-logo_white.png",
+  "./public/img/hhz-logo_white.png",
   " ",
   "Takin' It Back",
   "https://github.com/Jihefel/jeu_paires/blob/gh-pages/public/audio/Headhunterz%20-%20Takin%20It%20Back.mp3?raw=true"
   );
   
 export const liveAndLetDie = new Musique(
-  "../../public/img/BASS-MODULATORS_LOGOTYPE.png",
+  "./public/img/BASS-MODULATORS_LOGOTYPE.png",
   " ",
   "Live and Let Die",
   "https://github.com/Jihefel/jeu_paires/blob/gh-pages/public/audio/Bass%20Modulators%20-%20Live%20And%20Let%20Die.mp3?raw=true"
 );
 
 export const dreams200 = new Musique(
-  "../../public/img/noisecontrollers-logo-1.png",
-  "../../public/img/wildstylez-logo-white.png",
+  "./public/img/noisecontrollers-logo-1.png",
+  "./public/img/wildstylez-logo-white.png",
   "200 Dreams",
   "https://github.com/Jihefel/jeu_paires/blob/gh-pages/public/audio/Noisecontrollers%20&%20Wildstylez%20-%20200%20Dreams%20(Official%20Videoclip).mp3?raw=true"
 );
 
 export const intoTheWild = new Musique(
-  "../../public/img/wildstylez-logo-white.png",
+  "./public/img/wildstylez-logo-white.png",
   " ",
   "Into the Wild",
   "https://github.com/Jihefel/jeu_paires/blob/gh-pages/public/audio/Wildstylez%20-%20Into%20The%20Wild%20(Feat.%20KiFi)%20(Official%20Videoclip).mp3?raw=true"
 );
 
 export const journey = new Musique(
-  "../../public/img/project-one-logo-white-long.png",
+  "./public/img/project-one-logo-white-long.png",
   " ",
   "Journey of the Mind",
   "https://github.com/Jihefel/jeu_paires/blob/gh-pages/public/audio/Project%20One%20-%20Journey%20Of%20The%20Mind.mp3?raw=true"
 );
 
 export const oxygen = new Musique(
-  "../../public/img/hhz-logo_white.png",
+  "./public/img/hhz-logo_white.png",
   " ",
   "Oxygen",
   "https://github.com/Jihefel/jeu_paires/blob/gh-pages/public/audio/Headhunterz%20-%20Oxygen%20(Official%20Videoclip).mp3?raw=true"
 )
 
 export const untamable = new Musique(
-  "../../public/img/wildstylez-logo-white.png",
-  "../../public/img/sound-rush-logo.png",
+  "./public/img/wildstylez-logo-white.png",
+  "./public/img/sound-rush-logo.png",
   "Untamable",
   "https://github.com/Jihefel/jeu_paires/blob/gh-pages/public/audio/Untamable%20(feat.%20Ruby%20Prophet).mp3?raw=true"
 );
 
 export const beforeIWake = new Musique(
-  "../../public/img/hhz-logo_white.png",
-  "../../public/img/vertile-logo.png",
+  "./public/img/hhz-logo_white.png",
+  "./public/img/vertile-logo.png",
   "Before I Wake",
   "https://github.com/Jihefel/jeu_paires/blob/gh-pages/public/audio/Headhunterz%20&%20Vertile%20-%20Before%20I%20Wake%20(Official%20Videoclip).mp3?raw=true"
 );
 
 export const leapOfFaith = new Musique(
-  "../../public/img/hhz-logo_white.png",
+  "./public/img/hhz-logo_white.png",
   " ",
   "Leap of Faith",
   "https://github.com/Jihefel/jeu_paires/blob/gh-pages/public/audio/Leap%20Of%20Faith.mp3?raw=true"
 );
 
 export const holdMeClose = new Musique(
-  "../../public/img/arize-logo.png",
+  "./public/img/arize-logo.png",
   " ",
   "Hold Me Close",
   "https://github.com/Jihefel/jeu_paires/blob/gh-pages/public/audio/Hold%20Me%20Close.mp3?raw=true"
